2ee3d03702f78110f05afe3fd54620927c4015e0
Divya <singhdivya10aug2002@gmail.com>
2023-12-06 16:50:33 +0530

    Resume in React

---
+++ DAY 4/resume-in-react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+
+import Sidenav from './components/Sidenav'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Sidenav />
+
+      {/* <Experience /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ DAY 4/resume-in-react/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <center>
+        <h1 class='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl mt-4 mb-8 ml-8'>
+          <span class='text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>
+            Divya
+          </span>{' '}
+          Student
+        </h1>
+        <p class='text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 ml-12 '>
+          Here is Introduction of Professional.
+        </p>
+      </center>
+    </>
+  )
+}
+
+export default Header
