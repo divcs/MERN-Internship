8af287122a0a0176e7940d778f527769d51bd5de
Divya <singhdivya10aug2002@gmail.com>
2023-12-12 11:59:23 +0530

    Learning establishing connection

---
+++ DAY 9/pimrg_internship_mern/backend/connectDB.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import colors from 'colors'
+
+const connectDB= async () =>{
+ try{
+  await 
+ }
+}
--- DAY 9/pimrg_internship_mern/backend/index.js
+++ DAY 9/pimrg_internship_mern/backend/index.js
@@ -1,47 +1,56 @@
-// console.log('hello')
+// // console.log('hello')
 
-// let a = 10,
-//   b = 20,
-//   c
-// c = a + b
+// // let a = 10,
+// //   b = 20,
+// //   c
+// // c = a + b
 
-// console.log('Sum is: ', c)
+// // console.log('Sum is: ', c)
+
+// import express from 'express'
+// import cors from 'cors'
+// import morgan from 'morgan'
+// import colors from 'colors'
+// import dotenv from 'dotenv'
+
+// // env config
+// // dotenv.config({path: '/'})
+// dotenv.config()
+
+// // endpoints are executed through this
+// // rest object
+// const app = express()
+
+// // middlewares
+// app.use(cors())
+// app.use(express.json())
+// app.use(morgan('dev'))
+
+// // routes
+// app.get('/', (req, res) => {
+//   res.status(200).send({
+//     message: 'Node Server',
+//   })
+// })
+
+// app.get('/test', (req, res) => {
+//   res.status(200).send({
+//     message: 'Node Server test',
+//   })
+// })
+
+// // Port
+// const PORT = process.env.PORT || 8080
+
+// // listen
+// app.listen(8080, () => {
+//   console.log('Server running '.bgCyan.white)
+// })
+
+//
 
 import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import colors from 'colors'
-import dotenv from 'dotenv'
-
-// env config
-// dotenv.config({path: '/'})
-dotenv.config()
-
-// endpoints are executed through this
-// rest object
 const app = express()
-
-// middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
-
-// routes
-app.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Node Server',
-  })
-})
-
-app.get('/test', (req, res) => {
-  res.status(200).send({
-    message: 'Node Server test',
-  })
-})
-// Port
-const PORT = process.env.PORT || 8080
-
-// listen
-app.listen(8080, () => {
-  console.log('Server running '.bgCyan.white)
+app.listen(8080, (req, res) => {
+  console.log('Server listening on port', req.port)
 })
